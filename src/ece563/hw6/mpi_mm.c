d56f1fe9539bef73e4985d0c85afa2409e5b4d81
adhitha <adhithadias27@gmail.com>
2021-03-05 02:47:01 -0500

    remove comments in mpi_mm and format code

--- src/ece563/hw6/mpi_mm.c
+++ src/ece563/hw6/mpi_mm.c
@@ -3,15 +3,12 @@
 #include <time.h>
 #include <mpi.h>
 
-#define ROWS 8
-#define COLS 8
+#define ROWS 1600
+#define COLS 1600
 
-void printArray(double *a, int rows, int cols)
-{
-	for (int i = 0; i < rows; i++)
-	{
-		for (int j = 0; j < cols; j++)
-		{
+void printArray(double *a, int rows, int cols) {
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
 			printf("%.2f ", *(a + i * cols + j));
 		}
 		printf("\n");
@@ -23,19 +20,15 @@ double *makeArray(int rows, int cols, int value, int p, int stripeSize, int is_b
 {
 	double *arr = (double *)malloc(rows * cols * sizeof(double));
 
-	for (int r = 0; r < rows; r++)
-	{
-		for (int c = 0; c < cols; c++)
-		{
+	for (int r = 0; r < rows; r++) {
+		for (int c = 0; c < cols; c++) {
 			if (value == -1) {
 				if(is_b) {
 					*(arr + r * cols + c) = (double)((ROWS+1)*(c+2*p));
 				} else {
 					*(arr + r * cols + c) = (double)((ROWS+1)*c);
 				}
-			}
-			else
-			{
+			} else {
 				*(arr + r * cols + c) = value;
 			}
 		}
@@ -44,42 +37,23 @@ double *makeArray(int rows, int cols, int value, int p, int stripeSize, int is_b
 	return arr;
 }
 
-int min(int i, int j)
-{
+int min(int i, int j) {
 	return i < j ? i : j;
 }
 
-void MatrixMul(double *c, double *a, double *b, int stripeSize, int msg_from, int p)
-{
-	// if (p == 1) {
-	// 			printArray(a, stripeSize, COLS);
-	// 	printArray(b, COLS, stripeSize);
-	// }
-	
-	for (int i = 0; i < stripeSize; i++)
-	{
-		for (int j = 0; j < stripeSize; j++)
-		{
+void MatrixMul(double *c, double *a, double *b, int stripeSize, int msg_from, int p) {
+	for (int i = 0; i < stripeSize; i++) {
+		for (int j = 0; j < stripeSize; j++) {
 			double comp = 0.;
-			for (int k = 0; k < COLS; k++)
-			{
-				// if (p == 1) {
-				// 	printf("a: %f, b: %f\n", *(a + i * COLS + k), *(b + k * stripeSize + j));
-				// }
+			for (int k = 0; k < COLS; k++) {
 				comp += *(a + i * COLS + k) * *(b + k * stripeSize + j);
 			}
-			// if (p == 1) {
-			// 	printf("loc: %d, comp: %f\n\n\n", i * COLS + j, comp);
-			// }
-			
-			// *(c + i*COLS + j) = comp;
 			*(c + i * COLS + j + msg_from*stripeSize) = comp;
 		}
 	}
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
 	MPI_Init(&argc, &argv);
 
@@ -101,19 +75,8 @@ int main(int argc, char *argv[])
 	double t1, t2;
 	t1 = MPI_Wtime();
 
-	if (p == 1)
-	{
-		printArray(a, stripeSize, COLS);
-		printArray(b, ROWS, stripeSize);
-	}
-
 	// initial matrix multiplication
 	MatrixMul(c, a, b, stripeSize, msg_from, p);
-
-	if (p == 1)
-	{
-		printArray(c, stripeSize, COLS);
-	}
 
 	int received_from = p;
 
@@ -123,8 +86,7 @@ int main(int argc, char *argv[])
 	int size = ROWS * stripeSize;
 
 	msg_from = p - 1;
-	if (msg_from < 0)
-	{
+	if (msg_from < 0) {
 		msg_from = world_size + msg_from;
 	}
 	int send_to = (p + 1) % world_size;
@@ -146,8 +108,6 @@ int main(int argc, char *argv[])
 		if (p % 2 != 0) {
 			MPI_Recv(received_b, size, MPI_DOUBLE, msg_from, 0, MPI_COMM_WORLD,
 					 MPI_STATUS_IGNORE);
-			// printf("Process %d received token %f, t %f from process %d\n", p, received_b[0],
-			// 	   received_b[1], msg_from);
 		} else {
 			MPI_Send(b, size, MPI_DOUBLE, send_to, 0,
 					 MPI_COMM_WORLD);
@@ -157,36 +117,22 @@ int main(int argc, char *argv[])
 		received_b = b;
 		b = temp;
 
-		if (p == 1)
-		{
-			printf("Process %d received token %f, t %f from process %d\n", p, received_b[0],
-				received_b[1], received_from);
-			printArray(b, ROWS, stripeSize);
-		}
-
 		// matrix multiplication after receives
 		MatrixMul(c, a, b, stripeSize, received_from, p);
 		
-		// if (p == 1)
-		// {
-		// 	printArray(c, stripeSize, ROWS);
-		// }
 	}
-
-	// if (p == 1)
-	// {
-	// 	printArray(c, stripeSize, COLS);
-	// 	printArray(c, ROWS, stripeSize);
-	// }
 
 	double *final_c = makeArray(ROWS, COLS, 0, p, stripeSize, 0);
 	MPI_Gather(c, size, MPI_DOUBLE, final_c, size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
 	MPI_Barrier(MPI_COMM_WORLD);
 	t2 = MPI_Wtime();
-	printf("Elapsed time is %f\n", t2 - t1);
 
-	printArray(final_c, ROWS, COLS);
+	if (p == 0) {
+		printf("Elapsed time is %f\n", t2 - t1);
+	}
+	
+	// printArray(final_c, ROWS, COLS);
 
 	MPI_Finalize();
 	return EXIT_SUCCESS;
